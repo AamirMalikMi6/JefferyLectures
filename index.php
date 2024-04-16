517b43aed5ecdd93b42a2c88324459d860dc9f69
AamirMalikMi6 <m.aamir.malik.mi6@gmail.com>
2024-04-16 20:33:36 +0500

    Lecture 20

--- index.php
+++ index.php
@@ -1,4 +1,29 @@
 <?php
 
 require 'functions.php';
-require 'router.php';
+// require 'Tasks/task1.php';
+require 'Database.php';
+// require 'router.php';
+
+//connect to the database
+$config = require('config.php');
+
+
+
+
+$db = new Database($config['database']);
+
+$id = $_GET['id'];
+
+$query = "select * from posts where id=:id";
+
+$posts = $db->query($query, [':id' => $id])->fetch();
+// foreach ($posts as $post){
+//     echo "<li>" . $post['title'] ."</li>";
+// }
+
+dd($posts);
+
+
+
+    
